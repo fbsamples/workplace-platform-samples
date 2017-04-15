d518da8e80034e24e36a9272eae2c24373207df8
Connor Treacy <connor@fb.com>
2017-04-14 21:45:18 -0700

    Copyright Header

--- AccountManagement/csv_header.py
+++ AccountManagement/csv_header.py
@@ -1,3 +1,9 @@
+# Copyright 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
 #user creation header columns(lowercase it all)
 EMAIL_HEADER = 'email'
 FIRST_NAME_HEADER = 'first name'
