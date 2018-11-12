548cf8bed587130d975d2cbf5115d4ed36e02838
Nico Cinefra <ncinefra@fb.com>
2018-11-12 15:57:35 +0000

    Added PowerShell Scripts

---
+++ SamplePSScripts/ExportGroupMembers/README.md
@@ -0,0 +1,39 @@
+# Export members from a group
+
+This PowerShell script allows to export the members of a Workplace group to a XLSX file.
+
+## Setup
+
+* Create a new Custom Integration in the Workplace Admin Panel: [Create a custom Integration](https://developers.facebook.com/docs/workplace/custom-integrations-new/#creating).<br/>This requires at least "Manage Groups" and "Read User Emails" permissions. Take note of the Access Token.
+
+* Create a file named `accessToken.js` with the following content:
+
+   ```javascript
+   {
+         "accessToken" : "YOUR-ACCESS-TOKEN"
+   }
+   ``` 
+ 
+ * Find your GroupId. Go in a browser to the Workplace group you would like to extract members from and take note of the GroupId from the URL you see in the browser bar:
+ 
+   ```powershell
+   https://INSTANCE-NAME.facebook.com/groups/GROUP-ID
+   ```
+ 
+## Run
+
+* Run the script by passing the GroupId and `accessToken.js` file as input:
+
+   ```powershell
+   ./exportGroupMembers.ps1 -GroupId THIS-IS-A-GROUP-ID -WPAccessToken accessToken.js
+   ```
+
+   Here are the details of the passed params:
+
+   | Parameter         | Description                                                |  Type    |  Required    | 
+   |:-----------------:|:----------------------------------------------------------:|:--------:|:------------:|
+   | GroupId           |  The ID of the group                                       | _String_ | Yes          |
+   | WPAccessToken     |  The path for the JSON file with the access token          | _String_ | Yes          |
+   
+* A file named `members-[GroupId].xlsx` will be created in the same folder where your run the script. 
+   It will have `Name`, `Id`, `Email`, `Administator` columns.
---
+++ SamplePSScripts/ExportGroupMembers/exportGroupMembers.ps1
@@ -0,0 +1,62 @@
+param(
+    [Parameter(Mandatory=$true, HelpMessage='The ID of the Workplace Group you would like to export')] [string]$GroupId,
+    [Parameter(Mandatory=$true, HelpMessage='Path for your Workplace access token in .json format {"accessToken" : 123xyz}')] [string]$WPAccessToken
+)
+
+#Install ImportExcel Module
+If(!(Get-module ImportExcel)){Install-Module ImportExcel -scope CurrentUser}
+
+#Read JSON Access Token
+try {
+    $global:token = (Get-Content $WPAccessToken | Out-String | ConvertFrom-Json -ErrorAction Stop).accessToken
+    Write-Host -NoNewLine "Access Token JSON File: "
+    Write-Host -ForegroundColor Green "OK, Read!"
+}
+catch {
+    #Handle exception when passed file is not JSON
+    Write-Host -ForegroundColor Red "Fatal Error when reading JSON file. Is it correctly formatted? {'accessToken' : 123xyz}"
+    exit;
+}
+
+#Get specific group in the community via Graph API
+try {
+    $global:members = @()
+    $next = "https://graph.facebook.com/$GroupId/members/?fields=name,id,email,administrator"
+    do {
+        $results = Invoke-RestMethod -Uri ($next) -Headers @{Authorization = "Bearer " + $global:token}
+        if ($results) {
+            $global:members += $results.data
+            if($results.paging.cursors.after) {
+                $after = $results.paging.cursors.after
+                $next = "https://graph.facebook.com/$GroupId/members/?fields=name,id,email,administrator&after=$after"
+            }
+            else {$next = $null}
+        }
+        else {$next = $null}
+    } while($next) 
+} catch {
+    #Handle exception when having errors from Graph API
+    Write-Host -ForegroundColor Red "Fatal Error when getting group members from API. Is the GroupId you passed correct? Are API permissions correct?"
+    exit;
+}
+
+#Add members to XLSX
+try {
+    #Clean email-less fields
+    $($global:members | Where-Object {$_.email.endswith("emailless.facebook.com")}).email = $null
+    #Format property names
+    $global:members | `
+    ForEach-Object -Process {$_} | `
+    Select-Object -property `
+        @{N='Full Name';E={$_.name}}, `
+        @{N='Id';E={$_.id}}, `
+        @{N='Email';E={$_.email}}, `
+        @{N='Administrator';E={$_.Administrator}} | `
+        Export-Excel "./members-$GroupId.xlsx" -NoNumberConversion *
+    Write-Host -NoNewLine "Users written to XLSX: "
+    Write-Host -ForegroundColor Green "OK, Written!"
+} catch {
+    #Handle exception when writing to output XLSX
+    Write-Host -ForegroundColor Red "Fatal Error when writing to XLSX file!"
+    exit;
+}
