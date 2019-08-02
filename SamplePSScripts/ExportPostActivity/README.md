12d0e4203ad33ca2ae1bff218932de7add58cdc6
Nico Cinefra <ncinefra@fb.com>
2019-08-02 12:27:00 +0100

    exportPostActivity Added

---
+++ SamplePSScripts/ExportPostActivity/README.md
@@ -0,0 +1,40 @@
+# Export members from a group
+
+This PowerShell script allows to export the members of a Workplace group to a XLSX file.
+
+## Setup
+
+* Create a new Custom Integration in the Workplace Admin Panel: [Create a custom Integration](https://developers.facebook.com/docs/workplace/custom-integrations-new/#creating).<br/>This requires at least "Read Group Content", "Read Work Profile" and "Read User Email Address" permissions. Take note of the Access Token.
+
+* Create a file named `accessToken.js` with the following content:
+
+   ```javascript
+   {
+         "accessToken" : "YOUR-ACCESS-TOKEN"
+   }
+   ``` 
+ 
+ * Find your GroupId and PostId. Navigate to the permalink of the post you would like to extract Analytics for, take note of the GroupId and PostId from the URL you see in the browser bar:
+ 
+   ```powershell
+   https://INSTANCE-NAME.workplace.com/groups/GROUP-ID/permalink/POST-ID
+   ```
+ 
+## Run
+
+* Run the script by passing the GroupId and `accessToken.js` file as input:
+
+   ```powershell
+   ./exportPostActivity.ps1 -PostId {GroupId}_{PostId} -WPAccessToken accessToken.js -StartDate DD-MM-YYYY
+   ```
+
+   Here are the details of the passed params:
+
+   | Parameter            | Description                                                       |  Type    |  Required    | 
+   |:--------------------:|:-----------------------------------------------------------------:|:--------:|:------------:|
+   | {GroupId}_{PostId}   |  Concatenate GroupId and PostId with a _                          | _String_ | Yes          |
+   | WPAccessToken        |  The path for the JSON file with the access token                 | _String_ | Yes          |
+   | StartDate            |  The date starting from we need to extract contents, typically is the date of the post (DD-MM-YYYY) | _String_ | Yes          |
+   
+* A file named `stats-[GroupID]_[PostID].xlsx` will be created in the same folder where your run the script. 
+   It will include general stats about the post and a list of all the comments/replies and an analysis of relative reactions.
---
+++ SamplePSScripts/ExportPostActivity/exportPostActivity.ps1
@@ -0,0 +1,163 @@
+param(
+    [Parameter(Mandatory=$true, HelpMessage='The ID of the post in Workplace you would like to export comments for')] [string]$PostId,
+    [Parameter(Mandatory=$true, HelpMessage='The start date you would like to extract comments from. Format: DD-MM-YYYY, will default to Today')] [string]$StartDate,
+    [Parameter(Mandatory=$true, HelpMessage='Path for your Workplace access token in .json format {"accessToken" : 123xyz}')] [string]$WPAccessToken
+)
+
+#Install ImportExcel Module
+If(!(Get-module ImportExcel)){Install-Module ImportExcel -scope CurrentUser}
+
+#Read JSON Access Token
+try {
+    $global:token = (Get-Content $WPAccessToken | Out-String | ConvertFrom-Json -ErrorAction Stop).accessToken
+    Write-Host -NoNewLine "Access Token JSON File: "
+    Write-Host -ForegroundColor Green "OK, Read!"
+}
+catch {
+    #Handle exception when passed file is not JSON
+    Write-Host -ForegroundColor Red "Fatal Error when reading JSON file. Is it correctly formatted? {'accessToken' : 123xyz}"
+    exit;
+}
+
+#Set the date
+If($StartDate) {
+    try {
+        $date = $StartDate.split('-')
+        $global:startdate = ([System.DateTimeOffset](Get-Date -Day $date[0] -Month $date[1] -Year $date[2])).ToUnixTimeSeconds()
+    }
+    catch {
+        #Handle exception when having errors from Graph API
+        Write-Host -ForegroundColor Red "Fatal Error when reading date. Is the StartDate you passed a valid one?"
+        exit;  
+    }
+} Else {
+    $global:startdate = [int](Get-Date -UFormat %s -Millisecond 0)
+}
+
+$global:stats = @{
+    total_post_comments = 0;
+    total_post_replies = 0;
+    total_post_shares = 0;
+    total_post_seen_by = 0;
+    total_post_reactions = @{
+        like = 0;
+        love = 0;
+        haha = 0;
+        wow = 0;
+        sad = 0;
+        angry = 0;
+    };
+}
+
+#Get post stats
+Write-Host "Retrieving post stats from Workplace..."
+try {
+    $statsURL = "https://graph.workplace.com/$PostId/?fields=seen.limit(0).summary(total_count),likes.limit(0).summary(total_count),reactions.type(LOVE).limit(0).summary(total_count).as(reactions_love),reactions.type(WOW).limit(0).summary(total_count).as(reactions_wow),reactions.type(HAHA).limit(0).summary(total_count).as(reactions_haha),reactions.type(SAD).limit(0).summary(total_count).as(reactions_sad),reactions.type(ANGRY).limit(0).summary(total_count).as(reactions_angry)"
+    $results = Invoke-RestMethod -Uri ($statsURL) -Headers @{Authorization = "Bearer " + $global:token} -UserAgent "WorkplaceScript/ExportPostStats"
+        if ($results) {
+            $global:stats.total_post_seen_by = $results.seen.summary.total_count;
+            $global:stats.total_post_reactions.like = $results.likes.summary.total_count;
+            $global:stats.total_post_reactions.love = $results.reactions_love.summary.total_count;
+            $global:stats.total_post_reactions.haha = $results.reactions_haha.summary.total_count;
+            $global:stats.total_post_reactions.wow = $results.reactions_wow.summary.total_count;
+            $global:stats.total_post_reactions.sad = $results.reactions_sad.summary.total_count;
+            $global:stats.total_post_reactions.angry = $results.reactions_angry.summary.total_count;
+        }
+} catch {
+    #Handle exception when having errors from Graph API
+    Write-Host -ForegroundColor Red "Fatal Error when getting post stats from API. Is the PostId you passed correct? Are API permissions correct?"
+    exit;
+}
+
+#Get comments for posts
+Write-Host "Retrieving comments and replies from Workplace..."
+try {
+    $global:comments = @()
+    $next = "https://graph.workplace.com/$PostId/comments/?fields=created_time,from{name,id,email,primary_address,department},message,id,likes.limit(0).summary(total_count),reactions.type(LOVE).limit(0).summary(total_count).as(reactions_love),reactions.type(WOW).limit(0).summary(total_count).as(reactions_wow),reactions.type(HAHA).limit(0).summary(total_count).as(reactions_haha),reactions.type(SAD).limit(0).summary(total_count).as(reactions_sad),reactions.type(ANGRY).limit(0).summary(total_count).as(reactions_angry),comments.limit(0).summary(total_count)&order=chronological&since=$global:startdate"
+    do {
+        $results = Invoke-RestMethod -Uri ($next) -Headers @{Authorization = "Bearer " + $global:token} -UserAgent "WorkplaceScript/ExportPostStats"
+        if ($results) {
+            $results.data.ForEach({
+                $_ | Add-Member -NotePropertyName "Interaction" -NotePropertyValue "Comment" 
+                $global:comments += $_
+                $global:stats.total_post_comments++
+                $nextReply = "https://graph.workplace.com/" + $_.id + "/comments/?fields=created_time,from{name,id,email,primary_address,department},message,id,likes.limit(0).summary(total_count),reactions.type(LOVE).limit(0).summary(total_count).as(reactions_love),reactions.type(WOW).limit(0).summary(total_count).as(reactions_wow),reactions.type(HAHA).limit(0).summary(total_count).as(reactions_haha),reactions.type(SAD).limit(0).summary(total_count).as(reactions_sad),reactions.type(ANGRY).limit(0).summary(total_count).as(reactions_angry),comments.limit(0).summary(total_count)&order=chronological"
+                do {
+                    $resultsReplies = Invoke-RestMethod -Uri ($nextReply) -Headers @{Authorization = "Bearer " + $global:token} -UserAgent "WorkplaceScript/ExportPostStats"
+                    if($resultsReplies){
+                        $resultsReplies.data | ForEach-Object -Process {$_ | Add-Member -NotePropertyName "Interaction" -NotePropertyValue "Comment-Reply"}
+                        $global:comments += $resultsReplies.data
+                        $global:stats.total_post_replies += $resultsReplies.data.count
+                        if($resultsReplies.paging.cursors.after) {
+                            $afterReplies = $resultsReplies.paging.cursors.after
+                            $nextReply = "https://graph.workplace.com/" + $_.id + "/comments/?fields=created_time,from{name,id,email,primary_address,department},message,id,likes.limit(0).summary(total_count),reactions.type(LOVE).limit(0).summary(total_count).as(reactions_love),reactions.type(WOW).limit(0).summary(total_count).as(reactions_wow),reactions.type(HAHA).limit(0).summary(total_count).as(reactions_haha),reactions.type(SAD).limit(0).summary(total_count).as(reactions_sad),reactions.type(ANGRY).limit(0).summary(total_count).as(reactions_angry),comments.limit(0).summary(total_count)&order=chronological&after=$afterReplies"
+                        }
+                        else {$nextReply = $null}
+                    }
+                    else {$nextReply = $null}
+                } while($nextReply)
+            })
+            if($results.paging.cursors.after) {
+                $after = $results.paging.cursors.after
+                $next = "https://graph.workplace.com/$PostId/comments/?fields=created_time,from{name,id,email,primary_address,department},message,id,likes.limit(0).summary(total_count),reactions.type(LOVE).limit(0).summary(total_count).as(reactions_love),reactions.type(WOW).limit(0).summary(total_count).as(reactions_wow),reactions.type(HAHA).limit(0).summary(total_count).as(reactions_haha),reactions.type(SAD).limit(0).summary(total_count).as(reactions_sad),reactions.type(ANGRY).limit(0).summary(total_count).as(reactions_angry),comments.limit(0).summary(total_count)&order=chronological&since=$global:startdate&after=$after"
+            }
+            else {$next = $null}
+        }
+        else {$next = $null}
+    } while($next) 
+} catch {
+    #Handle exception when having errors from Graph API
+    Write-Host -ForegroundColor Red "Fatal Error when getting post comments from API. Is the PostId you passed correct? Are API permissions correct?"
+    exit;
+}
+
+#Add comments to XLSX
+try {
+    
+    $xlsxFile = "./stats-$PostId.xlsx" 
+    $reportTitle = "Post Analytics ($PostId)"
+
+    $xlp = $global:stats | `
+    Select-Object -property `
+    @{N='Total Post Comments';E={$global:stats.total_post_comments}}, `
+    @{N='Total Post Replies';E={$global:stats.total_post_replies}}, `
+    @{N='Total Post Shares';E={$global:stats.total_post_shares}}, `
+    @{N='Total Post Seen By';E={$global:stats.total_post_seen_by}} |`
+    Export-Excel -Path $xlsxFile -NoNumberConversion * -WorkSheetname PostAnalytics -StartRow 1 -PassThru
+
+    $xlp = $global:stats | `
+    Select-Object -property `
+    @{N='Total Post Like';E={$_.total_post_reactions.like}}, `
+    @{N='Total Post Love';E={$_.total_post_reactions.love}}, `
+    @{N='Total Post Haha';E={$_.total_post_reactions.haha}}, `
+    @{N='Total Post Wow';E={$_.total_post_reactions.wow}}, `
+    @{N='Total Post Sad';E={$_.total_post_reactions.sad}}, `
+    @{N='Total Post Angry';E={$_.total_post_reactions.angry}} |`
+    Export-Excel -ExcelPackage $xlp -NoNumberConversion * -WorkSheetname PostAnalytics -StartRow 4 -PassThru
+
+    $global:comments | `
+    ForEach-Object -Process {$_} | `
+    Select-Object -property `
+        @{N='Full Name';E={$_.from.name}}, `
+        @{N='Id';E={$_.from.id}}, `
+        @{N='Email';E={$_.from.email}}, `
+        @{N='Location';E={$_.from.primary_address}}, `
+        @{N='Department';E={$_.from.department}}, `
+        @{N='Message';E={$_.message}}, `
+        @{N='Interaction';E={$_.Interaction}}, `
+        @{N='Likes';E={$_.likes.summary.total_count}}, `
+        @{N='Loves';E={$_.reactions_love.summary.total_count}}, `
+        @{N='Hahas';E={$_.reactions_haha.summary.total_count}}, `
+        @{N='Wows';E={$_.reactions_wow.summary.total_count}}, `
+        @{N='Sads';E={$_.reactions_sad.summary.total_count}}, `
+        @{N='Angrys';E={$_.reactions_angry.summary.total_count}}, `
+        @{N='Date';E={$_.created_time}} |`
+        Export-Excel -ExcelPackage $xlp -NoNumberConversion * -WorkSheetname PostAnalytics -TableName CommentsReplies -StartRow 8 -Show
+    
+    Write-Host -NoNewLine "Analytics written to XLSX: "
+    Write-Host -ForegroundColor Green "OK, Written!"
+} catch {
+    #Handle exception when writing to output XLSX
+    Write-Host -ForegroundColor Red "Fatal Error when writing to XLSX file!"
+    exit;
+}
