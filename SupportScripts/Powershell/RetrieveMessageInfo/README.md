02652713e135854120ca95484f33306fa7235738
Antiklesys <syselkitna@gmail.com>
2020-03-03 11:42:31 +0800

    Added search by date functionality
    Updated code to search by date and return picture content url if found as a message attachment.

--- SupportScripts/Powershell/RetrieveMessageInfo/README.md
+++ SupportScripts/Powershell/RetrieveMessageInfo/README.md
@@ -16,12 +16,12 @@ The returned user_id are the ones of the message sender and message recipients.
    {
          "accessToken" : "YOUR-ACCESS-TOKEN"
    }
-   ``` 
- 
+   ```
+
  * Retrieve the User Id of the message sender or receiver by heading to the Admin Panel -> People
  <br/>Select the desired user and click on their name to navigate to their profile page
  <br/>From URL of the user profile page, copy the user ID e.g. profile.php?id={USER ID}
-  
+
 ## Run
 
 * Run the script by passing the `accessToken.js` file as input:
@@ -31,13 +31,23 @@ The returned user_id are the ones of the message sender and message recipients.
    ```
 
    For example if I sent a message stating "This is a test, please ignore" <br/>
-   I can set -MessageContent "This is a test" and the script will retrieve all the messages I sent or received which are including the sentence "This is a test".
+   I can set -MessageContent "This is a test" and the script will retrieve all the messages I sent or received which are including the sentence "This is a test".   </br>
+   </br>
+   It is also possible to search by date instead by running the script as:
+
+      ```powershell
+   ./RetrieveMessageInfo.ps1 -WPAccessToken accessToken.js -WPUser <User ID> -MessageDate <YYYY-MM-DD>
+   ```
+   This will retrieve all messages sent by a specific user on the specified date. This search method is recommended when searching for pictures/gifs.
    </br>
    </br>
    Here are the details of the passed params:
 
-   | Parameter         | Description                                                |  Type    |  Required    | 
+   | Parameter         | Description                                                |  Type    |  Required    |
    |:-----------------:|:----------------------------------------------------------:|:--------:|:------------:|
    | WPAccessToken     |  The path for the JSON file with the access token          | _String_ | Yes          |
    | WPUser            |  The Workplace ID of the sender or receiver of the message | _String_ | Yes          |
-   | MessageContent    |  A few words contained in the message                      | _String_ | Yes          |
+   | MessageContent    |  A few words contained in the message                      | _String_ | No           |
+   | MessageDate       |  Date when the message was sent in format YYYY-MM-DD       | _String_ | No           |
+
+   *Note: Although MessageContent and MessageDate are not required parameters, the search will return no results unless at least one of them is specified.
--- SupportScripts/Powershell/RetrieveMessageInfo/RetrieveMessageInfo.ps1
+++ SupportScripts/Powershell/RetrieveMessageInfo/RetrieveMessageInfo.ps1
@@ -1,7 +1,8 @@
 param(
     [Parameter(Mandatory=$true, Position=0, HelpMessage='Path for your Workplace access token in .json format {"accessToken" : 123xyz}')] [string]$WPAccessToken,
     [Parameter(Mandatory=$true, Position=1, HelpMessage='Use ID of message sender or receiver:')] [string]$WPUser,
-    [Parameter(Mandatory=$true, Position=2, HelpMessage='Message content to look for:')] [string]$MessageContent
+    [Parameter(Mandatory=$false, Position=2, HelpMessage='Message content to look for:')] [string]$MessageContent,
+    [Parameter(Mandatory=$false, Position=3, HelpMessage='Search by date (format YYYY-MM-DD):')] [string]$MessageDate
 )
 #Read JSON Access Token
 try {
@@ -15,10 +16,11 @@ catch {
     exit;
 }
 
+if($MessageContent){Write-Host "Searching for messages containing: "$MessageContent}
+if($MessageDate){Write-Host "Searching for messages sent on: "$MessageDate}
+if(($MessageContent -eq "") -and($MessageDate -eq "")){Write-Host -ForegroundColor Red "Please search either by message content or message date"}
 
-Write-Host "Searching for messages containing: "$MessageContent
-
-$user_messages_url= "https://graph.facebook.com/"+$WPUser+"/conversations?fields=messages{message,from,to}"
+$user_messages_url= "https://graph.facebook.com/"+$WPUser+"/conversations?fields=messages{message,from,to,attachments,created_time}"
 $messages_results = Invoke-RestMethod -Uri ($user_messages_url) -Headers @{Authorization = "Bearer " + $global:token} -UserAgent "WorkplaceScript/RetrieveMessageInfo"
 $runonce = 1
 while(($messages_results.paging.next) -or ($runonce)){
@@ -28,18 +30,27 @@ while(($messages_results.paging.next) -or ($runonce)){
         $message_id = $message.id
         $message_sender= $message.from.id
         $sender_email = $message.from.email
+        $message_date = $message.created_time
+        $message_date = $message_date -replace "T(.+)",""
+        $message_attachment_type = $message.attachments.data.mime_type
+        $message_attachment = $message.attachments.data.image_data.url
         if($null -eq $sender_email){$sender_email ="Bot/Custom Integration-->"+$message.from.name}
-        if ($message_content -match $MessageContent){
+        if ((($MessageContent -ne "") -and ($message_content -match $MessageContent)) -or (($MessageDate -ne "") -and ($message_date -match $MessageDate))){
             $string_found = 1
             do {
-            Write-Host -NoNewLine -ForegroundColor Yellow "Found message: " 
-            Write-Host -NoNewLine -ForegroundColor White $message_content 
+            Write-Host -NoNewLine -ForegroundColor Yellow "Found message: "
+            if($message_content){Write-Host -NoNewLine -ForegroundColor White $message_content}else{Write-Host -NoNewline -ForegroundColor White $message_attachment_type}
             Write-Host -NoNewLine -ForegroundColor Yellow " sent by: "
             Write-Host -ForegroundColor White $sender_email
+            Write-Host -NoNewLine -ForegroundColor Yellow " on date: "
+            Write-Host -ForegroundColor White $message_date
+            if($message_attachment){
+                Write-Host -NoNewLine -ForegroundColor Yellow "Attached picture/gif: "
+                Write-Host -ForegroundColor White $message_attachment
+            }
             Write-Host -ForegroundColor Red "  *  Is this the correct message? (Press S/s to Select, [Enter] to Skip): "
             $askRes = Read-Host
             } while (!(($askRes -eq "") -Or ($askRes -eq "S") -Or ($askRes -eq "s")))
-
         if($askRes -eq "S" -Or $askRes -eq "s") {
             Write-Host -ForegroundColor Red "Message Urls:"
             $delete_url = "https://graph.facebook.com/"+$message_id+"?user="+$message_sender
@@ -51,7 +62,7 @@ while(($messages_results.paging.next) -or ($runonce)){
             Write-Host -ForegroundColor Green "  *  OK, message reviewed by user and info provided!"
             } elseif ($askRes.length -eq 0) {
                 Write-Host -ForegroundColor Green "  *  Message skipped as requested!"
-            } 
+            }
         }
     }
     if(!($messages_results.paging.next)){
@@ -59,7 +70,7 @@ while(($messages_results.paging.next) -or ($runonce)){
     }else{
         $messages_results = Invoke-RestMethod -Uri ($messages_results.paging.next) -Headers @{Authorization = "Bearer " + $global:token} -UserAgent "WorkplaceScript/RetrieveMessageInfo"
     }
-}   
+}
     if($string_found -ne 1){
-        Write-Host -ForegroundColor Red "No matches found for: "$MessageContent
+        Write-Host -ForegroundColor Red "No matches found."
     }
