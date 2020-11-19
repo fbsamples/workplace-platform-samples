1f92a7670a309509d17815cbf0cb937301a1a97c
Carlos Sanchez <csesena@fb.com>
2020-11-19 18:20:19 +0000

    Adding script to export the list of Workplace groups
    Script to get and export the list of groups from your Workplace instance

---
+++ SupportScripts/Python/GetGroupListWithDetails/README.md
@@ -0,0 +1,29 @@
+# Get and export group list with details
+  
+**Language:** Python v3.7
+
+## DESCRIPTION
+This usecase example shows how to get all [groups](https://developers.facebook.com/docs/workplace/reference/graph-api/group) from your Workplace instance and obtain the name, last activity date (date of the last comment or post), description, privacy, number of members and list of admins. Finally it exports them to a CSV file.
+
+## SETUP
+Edit the code to add the required parameters and then save the code as `export_groups.py`. Information about the parameters below.
+
+### PARAMETERS
+Here are the details of the script parameters to be replaced:
+
+   | Parameter         | Description                                                |  Type           |  Required    | 
+   |:-----------------:|:----------------------------------------------------------:|:---------------:|:------------:|
+   | access_token      |  The access token of the Workplace integration             | _String_ | Yes |
+
+### GENERATE ACCESS TOKEN
+More information on how to generate an access token on Workplace can be found in [this link](https://developers.facebook.com/docs/workplace/custom-integrations-new/). The integration should at least have the following permissions: "Read group content", "Manage group content", "Manage groups" and "Read group membership".
+
+## RUN
+
+Run the script in a command line as follows:
+
+```python
+python export_groups.py
+```
+
+It will print in console the data in dict format and it will also export it to a CSV file called `group_export.csv`.
---
+++ SupportScripts/Python/GetGroupListWithDetails/export_groups.py
@@ -0,0 +1,61 @@
+# Copyright 2020-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import requests
+import json
+import csv
+
+# Constants
+GRAPH_URL_PREFIX = 'https://graph.facebook.com/'
+COMMUNITY_SUFFIX = 'community'
+FIELDS_CONJ = '?fields=' 
+GROUPS_SUFFIX = '/groups'
+GROUP_FIELDS = 'id,name,admins,members.limit(0).summary(true),privacy,description,updated_time'
+JSON_KEY_DATA = 'data'
+JSON_KEY_PAGING = 'paging'
+JSON_KEY_NEXT = 'next'
+
+# Methods
+def getAllGroups(access_token):
+    endpoint  = GRAPH_URL_PREFIX + COMMUNITY_SUFFIX + GROUPS_SUFFIX + FIELDS_CONJ + GROUP_FIELDS
+    return getPagedData(access_token, endpoint, [])
+
+def getNotPagedData(access_token, endpoint):
+    headers = buildHeader(access_token)
+    result = requests.get(endpoint,headers=headers)
+    return json.loads(result.text)
+
+def getPagedData(access_token, endpoint, data):
+    headers = buildHeader(access_token)
+    result = requests.get(endpoint,headers=headers)
+    result_json = json.loads(result.text)
+    json_keys = result_json.keys()
+    if JSON_KEY_DATA in json_keys and len(result_json[JSON_KEY_DATA]):
+        data.extend(result_json[JSON_KEY_DATA])
+    if JSON_KEY_PAGING in json_keys and JSON_KEY_NEXT in result_json[JSON_KEY_PAGING]:
+        next = result_json[JSON_KEY_PAGING][JSON_KEY_NEXT]
+        if next:
+            getPagedData(access_token, next, data)
+    return data
+
+def buildHeader(access_token):
+    return {'Authorization': 'Bearer ' + access_token}
+
+def exportToCSV(group_list):
+    keys = group_list[0].keys()
+    with open('group_export.csv', 'w', newline='\n')  as file:
+        dict_writer = csv.DictWriter(file, fieldnames=keys, delimiter=',', quotechar='"', escapechar='\\', extrasaction='ignore')
+        dict_writer.writeheader()
+        dict_writer.writerows(group_list)
+
+
+## START
+access_token = 'access_token'
+
+group_data = getAllGroups(access_token)
+print (group_data)
+
+exportToCSV(group_data);
