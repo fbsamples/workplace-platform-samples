3e546a0206717332a1677d210ad0e9bb1e6dae46
Carlos Sanchez <csesena@fb.com>
2021-07-28 18:52:06 +0100

    Clarifying has_access flag in update frontline script
    Clarifying has_access flag in update frontline status - Python script

--- SupportScripts/Python/UpdateUserFrontlineStatus/update_frontline.py
+++ SupportScripts/Python/UpdateUserFrontlineStatus/update_frontline.py
@@ -12,20 +12,26 @@ import csv
 GRAPH_URL_PREFIX = 'https://graph.workplace.com/'
 
 # Variables
-access_token = 'replace_with_your_access_token'
+access_token = 'your_access_token'
 file_name = 'email_list_change.csv'
 
 # Methods
 def sendModificationRequest(access_token, endpoint, frontline_status, has_access):
     headers = buildHeader(access_token)
-    body = {"frontline": '{ "is_frontline": ' + frontline_status + ', "has_access": ' + has_access + ' }' }
+    if ('' != has_access):
+        body = {"frontline": '{ "is_frontline": ' + frontline_status + ', "has_access": ' + has_access + ' }' }
+    else:
+        body = {"frontline": '{ "is_frontline": ' + frontline_status + ' }' }
     result = requests.post(endpoint, headers=headers, data = body)
-    print (endpoint + ' changing frontline status to ' + frontline_status + ' and has_access to ' + has_access + ' -> ' + result.text)
+    result_console = endpoint + ' changing frontline status to ' + frontline_status
+    if ('' != has_access):
+        result_console += ' and has_access to ' + has_access + ' -> ' + result.text
+    print (result_console)
 
 def buildHeader(access_token):
     return {'Authorization': 'Bearer ' + access_token, "User-Agent": "GithubRep-UpdateFrontlineStatus"}
 
-def modifyUserFrontlineStatus(access_token, email, frontline_status, has_access):
+def modifyUserFrontlineStatus(access_token, email, frontline_status, has_access=''):
     endpoint  = GRAPH_URL_PREFIX + email
     sendModificationRequest(access_token, endpoint, frontline_status, has_access)
 
@@ -37,5 +43,5 @@ with open(file_name, newline='') as f:
     reader = csv.reader(f)
     next(reader) #Skip header
     for row in reader:
-        #replace row[2] with string 'true' if not using 'has_access' column in csv.
+        #replace row[2] with string 'true' or empty string if not using 'has_access' column in csv.
         modifyUserFrontlineStatus(access_token, row[0], row[1], row[2])
