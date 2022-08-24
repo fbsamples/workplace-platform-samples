912e76df73e2650c6d3d89c4bfd72e0bab6bc004
Antiklesys <syselkitna@gmail.com>
2022-08-24 19:59:35 +0800

    Added script to bulk unclaim accounts

---
+++ SupportScripts/Powershell/UnclaimInBulk/README.md
@@ -0,0 +1,34 @@
+# Set Workplace users to unclaimed status in bulk with a XSLX file in input
+
+This PowerShell script allows to unclaim Workplace users in bulk.
+
+## Setup
+
+* Create a new Custom Integration in the Workplace Admin Panel: [Create a custom Integration](https://developers.facebook.com/docs/workplace/custom-integrations-new/#creating).<br/>This requires at least "Manage Accounts" permissions. Take note of the Access Token.
+
+* Create a file named `accessToken.js` with the following content:
+
+   ```javascript
+   {
+         "accessToken" : "YOUR-ACCESS-TOKEN"
+   }
+   ```
+
+ * Export the list of users you'd like to set as unclaimed: As an admin, you can export users' details by going to the People Tab in the Admin Portal > Filter in the search bar "Account status" accordingly > Click on the three dots button on the right > Export employee information of {X} people.  A mail message with the XLSX file with the requested information will be sent to your email address.
+
+
+## Run
+
+* Run the script by passing the XLSX file and `accessToken.js` file as input:
+
+   ```powershell
+   ./activateInBulk.ps1 -WPExportedUsers workplace_users.xlsx -WPAccessToken accessToken.js -Interactive
+   ```
+
+   Here are the details of the passed params:
+
+   | Parameter         | Description                                                |  Type    |  Required    |
+   |:-----------------:|:----------------------------------------------------------:|:--------:|:------------:|
+   | WPExportedUsers   |  The path for the XLSX file with the exported user data    | _String_ | Yes          |
+   | WPAccessToken     |  The path for the JSON file with the access token          | _String_ | Yes          |
+   | Interactive       |  If the script should display a message for each operation | _Switch_ | No           |
---
+++ SupportScripts/Powershell/UnclaimInBulk/unclaimInBulk.ps1
@@ -0,0 +1,83 @@
+param(
+    [Parameter(Mandatory=$true, HelpMessage='Path of the user export with the list of the users you would like to activate')] [string]$WPExportedUsers,
+    [Parameter(Mandatory=$true, HelpMessage='Path for your Workplace access token in .json format {"accessToken" : 123xyz}')] [string]$WPAccessToken,
+    [switch]$Interactive
+    )
+
+#Install ImportExcel Module
+If(!(Get-module ImportExcel)){Install-Module ImportExcel -scope CurrentUser}
+
+#Read JSON Access Token
+try {
+    $global:token = (Get-Content $WPAccessToken | Out-String | ConvertFrom-Json -ErrorAction Stop).accessToken
+    Write-Host -NoNewLine "Access Token JSON File: "
+    Write-Host -ForegroundColor Green "OK, Read!"
+}
+catch {
+    #Handle exception when passed file is not JSON
+    Write-Host -ForegroundColor Red "Fatal Error when reading JSON file. Is it correctly formatted? {'accessToken' : 123xyz}"
+    exit;
+}
+
+#Read XLSX Export File
+try {
+    #Read users from XLSX file
+    $global:xslxUsers = Import-Excel -Path $WPExportedUsers
+    Write-Host -NoNewLine "Workplace Users File: "
+    Write-Host -ForegroundColor Green "OK, Read!"
+}
+catch {
+    #Handle exception when unable to read file
+    Write-Host -ForegroundColor Red "Fatal Error when reading XLSX file. Is it the Workplace users export file?"
+    exit;
+}
+
+#Init Counters
+$total = 0;
+$unclaimed = 0;
+$errors = 0;
+
+Foreach($u in $global:xslxUsers) {
+
+    $uid = $u."User Id"
+    $uname = $u."Full Name"
+    $total++
+
+    If($Interactive.IsPresent) {
+        Write-Host -NoNewLine [$uid/$uname] ->
+    }
+
+    #Craft a Body
+    $body = (@{
+        schemas=@("urn:ietf:params:scim:schemas:core:2.0:User");
+        invited=$true;
+        claimed=$false
+        } | ConvertTo-Json)
+
+    try {
+        #Update User via SCIM API
+        $fbuser = Invoke-RestMethod -Method PUT -URI ("https://scim.workplace.com/Users/" + $uid) -Headers @{Authorization = "Bearer " + $global:token} -UserAgent "WorkplaceScript/ActivateInBulk" -ContentType "application/json" -Body $body
+        If($Interactive.IsPresent) {
+            Write-Host -ForegroundColor Green " Unclaimed"
+        }
+        $unclaimed++
+    }
+    catch {
+        $errors++
+        # Dig into the exception and print error message
+        $status = $_.Exception.Response.StatusCode.value__
+        $err = $_.Exception.Response.StatusCode
+        $msg = $_.Exception.Message
+        If($Interactive.IsPresent) {
+            Write-Host -ForegroundColor Red " KO ($status): $err - $msg"
+        } Else {
+            Write-Host -NoNewLine [$uid/$uname] ->
+            Write-Host -ForegroundColor Red " KO ($status): $err - $msg"
+        }
+    }
+}
+
+Write-Host "---------------------------------------------------------------------------------------------------------"
+Write-Host -NoNewLine -ForegroundColor Yellow "Summary "
+Write-Host "- Total User: $total - Unclaimed ($unclaimed), Errors ($errors)"
+Write-Host "---------------------------------------------------------------------------------------------------------"
